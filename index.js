97a9291e8fd85f125b8c91eebb405e34caea2681
ronaldgalvao <ronaldgalvao11@hotmail.com>
2025-08-03 17:54:49 -0300

    Desafio solucionado com sucesso =)

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-let fruta = "banana"
-switch (fruta) {
-    case "laranja":
-        console.log("Suco de laranja")
-    case "banana":
-        console.log("Vitamina de banana")
-}
+let nomeHeroi = "Reaper"
+let xpHeroi = 7200
+
+    if (xpHeroi <= 1000)
+    {
+        console.log("O Herói de nome " + nomeHeroi, "está no nível de Ferro")
+    }
+    else if (xpHeroi >= 1001 && xpHeroi <= 2000)
+    {
+        console.log("O Herói de nome " + nomeHeroi, "está no nível de Bronze")
+    }
+    else if (xpHeroi >= 2001 && xpHeroi <= 5000)
+    { 
+        console.log("O Herói de nome " + nomeHeroi, "está no nível de Prata")
+    }
+    else if (xpHeroi >= 5001 && xpHeroi <= 7000)
+    {
+        console.log("O Herói de nome " + nomeHeroi, "está no nível de Ouro")
+    }       
+    else if (xpHeroi >= 7001 && xpHeroi <= 8000)
+    {
+        console.log("O Herói de nome " + nomeHeroi, "está no nível de Platina")
+    }
+    else if (xpHeroi >= 8001 && xpHeroi <= 9000)
+    {
+        console.log("O Herói de nome " + nomeHeroi, "está no nível de Ascendente")
+    }
+    else if (xpHeroi >= 9001 && xpHeroi <= 10000)
+    {
+        console.log("O Herói de nome " + nomeHeroi, "está no nível de Imortal")
+    }
+    else if (xpHeroi >= 10001)
+    {
+        console.log("O Herói de nome " + nomeHeroi, "está no nível de Radiante")
+    }
+    else
+    {
+        console.log ("O Herói tem um nível de XP incalculável.")
+    }
